859fd87326d0a7e2df47b0093373c72d50b602eb
Jake Choi <104010121+myWorld7588@users.noreply.github.com>
2023-02-03 16:46:38 -0500

    Update Request Header Parser Microservice.js

--- Request Header Parser Microservice/Request Header Parser Microservice.js
+++ Request Header Parser Microservice/Request Header Parser Microservice.js
@@ -1,3 +1,9 @@
+// Tests
+// You should provide your own project, not the example URL.
+// A request to /api/whoami should return a JSON object with your IP address in the ipaddress key.
+// A request to /api/whoami should return a JSON object with your preferred language in the language key.
+// A request to /api/whoami should return a JSON object with your software in the software key.
+
 // server.js
 // where your node app starts
 
