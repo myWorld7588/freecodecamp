7b606e4d5e3c6254a1232f76fe84e1cb33756ab1
Jake Choi <104010121+myWorld7588@users.noreply.github.com>
2023-02-04 20:22:40 -0500

    Update File Metadata Microservice.js

--- FCC_File Metadata Microservice/File Metadata Microservice.js
+++ FCC_File Metadata Microservice/File Metadata Microservice.js
@@ -38,7 +38,7 @@ app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
   }
 });
 
-
+// Listen for request
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
