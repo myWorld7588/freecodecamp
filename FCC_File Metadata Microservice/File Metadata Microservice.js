29994a1078168fa488951cd24129044f5cc03c5e
Jake Choi <104010121+myWorld7588@users.noreply.github.com>
2023-02-04 20:24:29 -0500

    Update File Metadata Microservice.js

--- FCC_File Metadata Microservice/File Metadata Microservice.js
+++ FCC_File Metadata Microservice/File Metadata Microservice.js
@@ -1,3 +1,11 @@
+// Tests
+
+// You should provide your own project, not the example URL.
+// You can submit a form that includes a file upload.
+// The form file input field has the name attribute set to upfile.
+// When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
+
+
 // initialize project
 var express = require('express');
 var cors = require('cors');
