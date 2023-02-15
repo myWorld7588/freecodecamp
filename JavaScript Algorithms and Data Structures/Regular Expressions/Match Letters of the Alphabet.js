ed6db2823d82b81c7642cb00d52beddd90ca8c91
Jake Choi <104010121+myWorld7588@users.noreply.github.com>
2023-02-15 16:33:10 -0500

    Update Match Letters of the Alphabet.js

--- JavaScript Algorithms and Data Structures/Regular Expressions/Match Letters of the Alphabet.js
+++ JavaScript Algorithms and Data Structures/Regular Expressions/Match Letters of the Alphabet.js
@@ -20,3 +20,7 @@ matStr.match(bgRegex);
 
 // Note: Be sure to match both uppercase and lowercase letters.
 
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/ig; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
