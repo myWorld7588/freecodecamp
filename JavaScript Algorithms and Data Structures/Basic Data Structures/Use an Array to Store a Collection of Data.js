b254a3a9fbe4c63e56d7e3830e9b22757b4838a7
Jake Choi <104010121+myWorld7588@users.noreply.github.com>
2023-02-23 16:07:40 -0500

    Create Use an Array to Store a Collection of Data.js

---
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/Use an Array to Store a Collection of Data.js
@@ -0,0 +1,8 @@
+// Use an Array to Store a Collection of Data
+
+// The below is an example of the simplest implementation of an array data structure. 
+// This is known as a one-dimensional array, meaning it only has one level, or that it does not have any other arrays nested within it. 
+// Notice it contains booleans, strings, and numbers, among other valid JavaScript data types:
+
+let simpleArray = ['one', 2, 'three', true, false, undefined, null];
+console.log(simpleArray.length);
