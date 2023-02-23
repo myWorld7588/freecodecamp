f11aac86cb23f1984b1535fd4792734315924947
Jake Choi <104010121+myWorld7588@users.noreply.github.com>
2023-02-23 16:15:23 -0500

    Update Use an Array to Store a Collection of Data.js

--- JavaScript Algorithms and Data Structures/Basic Data Structures/Use an Array to Store a Collection of Data.js
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/Use an Array to Store a Collection of Data.js
@@ -6,3 +6,38 @@
 
 let simpleArray = ['one', 2, 'three', true, false, undefined, null];
 console.log(simpleArray.length);
+
+// The console.log call displays 7.
+
+// All arrays have a length property, which as shown above, can be very easily accessed with the syntax Array.length. 
+// A more complex implementation of an array can be seen below. This is known as a multi-dimensional array, or an array that contains other arrays. 
+// Notice that this array also contains JavaScript objects, which we will examine very closely in our next section, but for now, all you need to know is that arrays are also capable of storing complex objects.
+
+let complexArray = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    }
+  ]
+];
+
+// We have defined a variable called yourArray. 
+// Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. 
+// Your array should contain at least one string, one number, and one boolean.
+
+let yourArray = ["one", "2", 3, false, null]; // Change this line
